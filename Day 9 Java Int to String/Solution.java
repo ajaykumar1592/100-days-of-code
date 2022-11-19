3a9ed80d1d73defe67ee39d0ec87f33e0d64ec65
ajaykumar1592 <114764694+ajaykumar1592@users.noreply.github.com>
2022-11-19 22:45:11 +0530

    Create Solution.java

---
+++ Day 9 Java Int to String/Solution.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+        String s =Integer.toString(n);
+        while(n==Integer.parseInt(s)){
+        
+            if(n == Integer.parseInt(s)) {
+                System.out.println("Good Job");
+            }
+            else {
+               System.out.println("Wrong answer."); 
+            }
+            
+        }
+    }
+}
+    
+

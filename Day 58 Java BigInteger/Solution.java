8f15674a0ef677e9f6f9e99f341387adc45e0478
ajaykumar1592 <114764694+ajaykumar1592@users.noreply.github.com>
2022-12-27 20:52:32 +0530

    Create Solution.java

---
+++ Day 58 Java BigInteger/Solution.java
@@ -0,0 +1,13 @@
+import java.io.*; 
+import java.util.*; 
+import java.math.BigInteger;
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+   BigInteger a = scan.nextBigInteger();
+   BigInteger b = scan.nextBigInteger();
+
+    System.out.println(a.add(b));
+    System.out.println(a.multiply(b));
+    
+}
+}
